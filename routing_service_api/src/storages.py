d649a92d03aa3d89d617b2c10f680121d0e197c4
lukin_ma <@>
2023-03-31 12:33:09 +0300

    Первое приближение

--- routing_service_api/src/storages.py
+++ routing_service_api/src/storages.py
@@ -1,5 +1,11 @@
 import boto3
 
+from geopy.distance import distance
+import geocoder
+from src.schemas import Storage
+
+from src.settings import settings
+
 
 class ObjectStorageBase:
 
@@ -33,6 +39,35 @@ class ObjectStorageBase:
                 'Bucket': self.bucket,
                 'Key': key
             },
-            ExpiresIn=3600*24  # set expiration time for 24 hour
+            ExpiresIn=3600 * 24  # set expiration time for 24 hour
         )
         return url
+
+
+class StorageWorker:
+    cdn_storages = []
+
+    def create_storage_list(self):
+        for i in range(1, settings.storages_count + 1):
+            storage_url = getattr(settings, f"storage{i}_url")
+            storage_access_key = getattr(settings, f"storage{i}_access_key")
+            storage_secret_key = getattr(settings, f"storage{i}_secret_key")
+            storage_ip = getattr(settings, f"storage{i}_ip")
+            storage = Storage(
+                url=storage_url,
+                ip=storage_ip,
+                access_key=storage_access_key,
+                secret_key=storage_secret_key
+            )
+            self.cdn_storages.append(storage)
+
+    def get_cdn(self, ip_address):
+        user_geo = geocoder.ip(ip_address)
+        min_dist = distance(geocoder.ip(self.cdn_storages[0].ip).latlng, user_geo.latlng).km
+        min_dist_index = 0
+        for i in range(1, len(self.cdn_storages)):
+            dist = distance(geocoder.ip(self.cdn_storages[i].ip).latlng, user_geo.latlng).km
+            if dist < min_dist:
+                min_dist = dist
+                min_dist_index = i
+        return self.cdn_storages[min_dist_index]
