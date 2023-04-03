ebb285b4789303abb5c9f9e75d12f6574b5f68d5
Sergey Morichev <s.morichev@yandex.ru>
2023-04-03 15:05:00 +0500

    use same minio config across services

--- cdn/routing_service/src/settings.py
+++ cdn/routing_service/src/settings.py
@@ -3,22 +3,22 @@ from pydantic import BaseSettings, Field
 
 class Settings(BaseSettings):
     storage1_url: str = Field("http://localhost:9000/", env="STORAGE1_URL")
-    storage1_access_key: str = Field("admin")
-    storage1_secret_key: str = Field("supersecret")
+    storage1_access_key: str = Field("admin", env="MINIO_USER")
+    storage1_secret_key: str = Field("supersecret", env="MINIO_PASSWORD")
     storage1_ip: str = Field("83.220.236.105")
 
-    storage2_url: str = Field("http://localhost:9003/", env="STORAGE2_URL")
-    storage2_access_key: str = Field("admin")
-    storage2_secret_key: str = Field("supersecret")
+    storage2_url: str = Field("http://localhost:9010/", env="STORAGE2_URL")
+    storage2_access_key: str = Field("admin", env="MINIO_USER")
+    storage2_secret_key: str = Field("supersecret", env="MINIO_PASSWORD")
     storage2_ip: str = Field("95.142.196.32")
 
-    storage3_url: str = Field("http://localhost:9005/", env="STORAGE3_URL")
-    storage3_access_key: str = Field("admin")
-    storage3_secret_key: str = Field("supersecret")
+    storage3_url: str = Field("http://localhost:9020/", env="STORAGE3_URL")
+    storage3_access_key: str = Field("admin", env="MINIO_USER")
+    storage3_secret_key: str = Field("supersecret", env="MINIO_PASSWORD")
     storage3_ip: str = Field("92.255.196.137")
 
-    bucket: str = Field("test")
-    storages_count = 1
+    bucket: str = Field("test", env="MOVIES_BUCKET")
+    storages_count = 3
 
     class Config:
         env_file = "../.env"
