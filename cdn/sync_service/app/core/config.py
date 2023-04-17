fb848f06931e78eb6cffa6ca2ae2d0d9e76556cd
Sergey Morichev <s.morichev@yandex.ru>
2023-04-17 18:32:11 +0500

    fix config

--- cdn/sync_service/app/core/config.py
+++ cdn/sync_service/app/core/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     DEBUG: bool = Field(True, env="SYNC_DEBUG")
     SECRET_KEY: str = Field("secret_key", env="SYNC_SECRET_KEY")
     # S3_SETTINGS для автоматического добавления хранилищ в dev или test окружении
-    S3_SETTINGS: list[S3Settings] = Field(..., env="SYNC_S3_HOSTS", default_factory=list)
+    S3_SETTINGS: list[S3Settings] = Field(env="SYNC_S3_HOSTS", default_factory=list)
     S3_MASTER_ID: str = Field("master", env="SYNC_S3_MASTER_ID")
     S3_FREE_SPACE_LIMIT: int = Field(..., env="SYNC_S3_FREE_SPACE_LIMIT")
     SYNC_HTTP_PATH: str = "/api/v1/sync"
