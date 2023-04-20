d1f7a8a7cdd3c95e981a4eb65b282d80bb978151
Sergey Morichev <s.morichev@yandex.ru>
2023-04-21 01:33:10 +0500

    change url and ip for demonstration

--- cdn/routing_service/src/storages.py
+++ cdn/routing_service/src/storages.py
@@ -3,11 +3,26 @@ import backoff as backoff
 import boto3
 import botocore.exceptions
 import geocoder
-import requests
 from geopy.distance import distance
 
 from src.schemas import Storage
 from src.settings import settings, logger
+
+
+# при запуске в dev окружении в компоузе урлы хранилищ отличаются для внутренних
+# юзеров (в компоузе) и внешних (на хосте)
+# подменяем урл для генерации ссылок TODO сделать нормальную реализацию
+change_urls = {
+    "http://minio-0:9000": "http://localhost:9000",
+    "http://minio-1:9000": "http://localhost:9010",
+    "http://minio-2:9000": "http://localhost:9020",
+}
+
+change_ip = {
+    "172.18.0.10": "109.252.76.233",  # Москва
+    "172.18.0.11": "178.162.102.173",  # Санкт Петербург
+    "172.18.0.12": "213.151.2.107",  # Екатеринбург
+}
 
 
 class ObjectStorageBase:
@@ -23,6 +38,17 @@ class ObjectStorageBase:
             aws_access_key_id=self.access_key,
             aws_secret_access_key=self.secret_key
         )
+        # только для демонстрации работы в dev окружении
+        url_for_links = change_urls.get(endpoint_url)
+        if url_for_links:
+            self.s3_for_links = boto3.client(
+                's3',
+                endpoint_url=url_for_links,
+                aws_access_key_id=self.access_key,
+                aws_secret_access_key=self.secret_key
+            )
+        else:
+            self.s3_for_links = self.s3
 
     def check_file(self, key):
         try:
@@ -33,7 +59,7 @@ class ObjectStorageBase:
             return
 
     def get_link_file(self, key):
-        url = self.s3.generate_presigned_url(
+        url = self.s3_for_links.generate_presigned_url(
             'get_object',
             Params={
                 'Bucket': self.bucket,
@@ -56,10 +82,14 @@ class StorageWorker:
                 response.raise_for_status()
                 storages = await response.json()
         for storage in storages:
+            # подменяем для демонстрации
+            ip_addr = change_ip.get(storage["ip_address"])
+            if not ip_addr:
+                ip_addr = storage["ip_address"]
             self.cdn_storages.append(
                 Storage(
                     url=storage["url"],
-                    ip=storage["ip_address"],
+                    ip=ip_addr,
                     access_key=settings.storage_access_key,
                     secret_key=settings.storage_secret_key
                 ))
